3ffbf6f41144ed6b0d8e6667c564fa2c8059fb71
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-01-09 12:43:46 +0100

    Manager class for spawning resource

---
+++ src/ResourceManager/ResourceSpawner.java
@@ -0,0 +1,13 @@
+package ResourceManager;
+
+/**
+ * Manager class for generating global resources.
+ * @author Marc Rohwedder Kær
+ * @date 09-01-2019
+ */
+public class ResourceSpawner implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
