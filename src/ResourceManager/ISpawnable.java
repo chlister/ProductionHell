283e6c1b3d0a524f209e22bc541961e1c6b8e7b1
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2019-01-10 08:57:24 +0100

    Resourcemanager added for handling all spawning of resource to the global deposit

---
+++ src/ResourceManager/ISpawnable.java
@@ -0,0 +1,11 @@
+package ResourceManager;
+
+/**
+ * Interface for adding a spawn method.
+ * @author Marc Rohwedder Kær
+ * @date 09-01-2019
+ */
+public interface ISpawnable {
+
+    void SpawnTimer();
+}
--- src/ResourceManager/ResourceSpawner.java
+++ src/ResourceManager/ResourceSpawner.java
@@ -1,13 +1,32 @@
 package ResourceManager;
+import Model.Resource.*;
 
 /**
  * Manager class for generating global resources.
  * @author Marc Rohwedder Kær
  * @date 09-01-2019
  */
-public class ResourceSpawner implements Runnable {
+public class ResourceSpawner implements Runnable, ISpawnable {
+    private static boolean running;
+    private double countdown;
+
+    /**
+     *
+     */
     @Override
     public void run() {
 
+        running = true;
+        // Start spawnTimer
+        SpawnTimer();
+    }
+
+    @Override
+    public void SpawnTimer() {
+        while (running){
+            // TODO: Create countdown for adding a resource to the pile
+
+            //TODO: check if interrupt then break the while loop
+        }
     }
 }
